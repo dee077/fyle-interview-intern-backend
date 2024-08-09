01920fff4c20a174a12ad45659e9f206b3ee5b72
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-08-09 17:06:34 +0530

    test coverage to 97

--- tests/home_test.py
+++ tests/home_test.py
@@ -1,3 +1,4 @@
+from core.libs.exceptions import FyleError
 
 def test_ready(client):
     response = client.get('/')
@@ -6,3 +7,18 @@ def test_ready(client):
     data = response.json
     assert data['status'] == 'ready'
     assert 'time' in data
+
+def test_page_not_found(client):
+    response = client.get('/any-endpoint')
+    assert response.status_code == 404
+    assert response.json['error'] == 'NotFound'
+
+def test_fyle_error_to_dict():
+    error_message = "This is a test error message"
+    status_code = 404
+    error = FyleError(status_code=status_code, message=error_message)
+    
+    error_dict = error.to_dict()
+    
+    assert error_dict == {'message': error_message}
+    assert error.status_code == status_code
